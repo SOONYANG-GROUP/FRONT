8c324f4a8c6e6dfbec5bff0670713cdd4bd0bf0f
coldsteelpope <coldsteelpope@gmail.com>
2023-03-18 16:09:26 +0900

    add cards

--- src/Pages/Champion/Champions.js
+++ src/Pages/Champion/Champions.js
@@ -7,7 +7,58 @@ const Champions = () => {
 
 
     return(
-        <div></div>
+        <div>
+
+
+
+<div class="container-fluid">
+
+		<div class="row">
+			<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">카드 제목</h5>
+						<p class="card-text">카드 내용</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">카드 제목</h5>
+						<p class="card-text">카드 내용</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">카드 제목</h5>
+						<p class="card-text">카드 내용</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">카드 제목</h5>
+						<p class="card-text">카드 내용</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">카드 제목</h5>
+						<p class="card-text">카드 내용</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+        </div>
     )    
 }
 
