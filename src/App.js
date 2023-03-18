71b65704558c2b3ce2a8a78e368b3d0f4f542fa9
coldsteelpope <coldsteelpope@gmail.com>
2023-03-18 17:15:59 +0900

    add dummy data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import CreateBattle from "./Pages/Battle/CreateBattle";
 import Header from "./Components/Header/Header";
 import Test from "./Test";
 
+import Battles from "./Pages/Battle/Battles";
 import Champions from "./Pages/Champion/Champions";
+
+import Battle from "./Pages/Battle/Battle";
+import Champion from "./Pages/Champion/Champion";
 
 import Dashboard from "./Pages/Admin/Dashboard";
 import CreateSkill from "./Pages/Admin/CreateSkill";
@@ -25,7 +29,11 @@ function App() {
         <Route path="/admin" element={<Dashboard />} />
         <Route path="/admin/create/skill" element={<CreateSkill />} />
         <Route path="/admin/create/champion" element={<CreateChampion />} />
+
         <Route path="/champions" element={<Champions />} />
+        <Route path="/champion/:id" element={<Champion />} />
+        <Route path="/battles" element={<Battles />} />
+        <Route path="/battle/:id" element={<Battle />} />
       </Routes>
     </Router>
   );
--- src/Pages/Battle/Battle.js
+++ src/Pages/Battle/Battle.js
@@ -1,32 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Loading from "../Loading";
 
-import ChampionsDummyData from "../../DummyData/Champion.json";
-
-const Champions = (props) => {
+const Battle = () => {
     const [ isLoading, setIsLoading ] = useState(true);
-    const [ champions, setChampions ] = useState([]);
-
+    
 
     useEffect(() => {
-        setChampions(ChampionsDummyData.champions);
         setIsLoading(false);
     }, []);
 
+    
+
     if(isLoading)
     {
-        return(
-            <Loading />
-        )
+        return(<Loading />)
     }
     else
     {
         return(
             <div>
-    
+                
             </div>
-        ) 
+        )
     }
 }
 
-export default Champions;
+export default Battle;
