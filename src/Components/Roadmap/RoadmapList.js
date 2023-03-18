a5851b078d4197bd08f44b626563e50c2277e131
Picka <marmong@naver.com>
2023-03-18 16:51:35 +0900

    update ProjectList & Roadmap list

--- src/Components/Roadmap/RoadmapList.js
+++ src/Components/Roadmap/RoadmapList.js
@@ -2,69 +2,77 @@ import RoadmapElement from "./RoadmapElement";
 
 const RoadmapList = () => {
   return (
-    <div
-      id="carouselExampleIndicators"
-      class="carousel slide"
-      data-bs-ride="true"
-    >
-      <div class="carousel-indicators">
+    <div id="Roadmap" className="carousel slide" data-bs-ride="true">
+      <div className="carousel-indicators">
         <button
           type="button"
-          data-bs-target="#carouselExampleIndicators"
+          data-bs-target="#Roadmap"
           data-bs-slide-to="0"
-          class="active"
+          className="active"
           aria-current="true"
           aria-label="Slide 1"
+          style={{ backgroundColor: "gray" }}
         ></button>
         <button
           type="button"
-          data-bs-target="#carouselExampleIndicators"
+          data-bs-target="#Roadmap"
           data-bs-slide-to="1"
           aria-label="Slide 2"
+          style={{ backgroundColor: "gray" }}
         ></button>
         <button
           type="button"
-          data-bs-target="#carouselExampleIndicators"
+          data-bs-target="#Roadmap"
           data-bs-slide-to="2"
           aria-label="Slide 3"
+          style={{ backgroundColor: "gray" }}
         ></button>
       </div>
-      <div class="carousel-inner" style={{ marginBottom: "40px" }}>
-        <div class="carousel-item active">
-          <div class="row">
-            <div class="col">
+      <div className="carousel-inner" style={{ marginBottom: "100px" }}>
+        <div className="carousel-item active">
+          <div
+            className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 "
+            style={{ marginBottom: "50px" }}
+          >
+            <div className="col">
               <RoadmapElement />
             </div>
-            <div class="col">
+            <div className="col">
               <RoadmapElement />
             </div>
-            <div class="col">
-              <RoadmapElement />
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div class="row">
-            <div class="col">
-              <RoadmapElement />
-            </div>
-            <div class="col">
-              <RoadmapElement />
-            </div>
-            <div class="col">
+            <div className="col">
               <RoadmapElement />
             </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <div class="row">
-            <div class="col">
+        <div className="carousel-item">
+          <div
+            className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 "
+            style={{ marginBottom: "50px" }}
+          >
+            <div className="col">
               <RoadmapElement />
             </div>
-            <div class="col">
+            <div className="col">
               <RoadmapElement />
             </div>
-            <div class="col">
+            <div className="col">
+              <RoadmapElement />
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <div
+            className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 "
+            style={{ marginBottom: "50px" }}
+          >
+            <div className="col">
+              <RoadmapElement />
+            </div>
+            <div className="col">
+              <RoadmapElement />
+            </div>
+            <div className="col">
               <RoadmapElement />
             </div>
           </div>
