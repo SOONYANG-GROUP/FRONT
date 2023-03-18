32b21ccaf7682bb23a6e7a3518ccf219530ac80f
coldsteelpope <coldsteelpope@gmail.com>
2023-03-18 15:57:56 +0900

    create champion

--- src/Components/List/SkillsList.js
+++ src/Components/List/SkillsList.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-const SkillsList = () => {
-    
+const SkillListElement = ({
+    index,
+    skill
+}) => {
+    return(
+        <div key={index}>
+            <h3>
+                {skill.name}
+            </h3>
+            <p>
+                {skill.description}
+            </p>
+        </div>
+    )
+}
+
+const SkillsList = ({
+    skills
+}) => {
+    return(skills.map((skill, index) => {
+        return(
+            <SkillListElement 
+                key={index}
+                skill={skill}
+            />
+        )
+    }))
 }
 
 export default SkillsList;
