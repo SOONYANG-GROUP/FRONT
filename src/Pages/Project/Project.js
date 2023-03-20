7aeab3e5b718976feef6d4ff0df8c6d052eb7203
Picka <marmong@naver.com>
2023-03-21 07:58:29 +0900

    Update Project.js

--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -8,24 +8,18 @@ import CommentList from "../../Components/List/CommentList";
 
 const Project = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [ creatingComment, setCreatingComment ] = useState(false);
-  const [ creatingToDoListEle, setCreatingToDoListEle ] = useState(false);
-<<<<<<< HEAD
-  const [ creatingDiscord, setCreatingDiscord ] = useState(false);
-  const [ creatingOpenKakao, setCreatingOpenKakao ] = useState(false);
-
-=======
->>>>>>> parent of 3329cfe (get accessToken, refreshToken)
+  const [creatingComment, setCreatingComment] = useState(false);
+  const [creatingToDoListEle, setCreatingToDoListEle] = useState(false);
 
   const [project, setProject] = useState({});
-  const [ comment, setComment ] = useState("");
-  const [ comments, setComments ] = useState([]);
-  const [ todoList, setTodoList ] = useState([]);
-  
-  const [ discordURL, setDiscordURL ] = useState("");
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-  const [ changingPage, setChangingPage ] = useState(false);
-  const [ pageNumber, setPageNumber ] = useState(0);
+  const [discordURL, setDiscordURL] = useState("");
+
+  const [changingPage, setChangingPage] = useState(false);
+  const [pageNumber, setPageNumber] = useState(0);
 
   const { id } = useParams();
 
@@ -45,15 +39,13 @@ const Project = () => {
 
   const onChangeComment = (e) => {
     setComment(e.target.value);
-  }
-
-  
+  };
 
   const onChangePageNumber = async (e) => {
     await setChangingPage(true);
     setPageNumber(parseInt(e.target.id));
     await setChangingPage(false);
-  }
+  };
 
   if (isLoading) {
     return <Loading />;
@@ -70,34 +62,49 @@ const Project = () => {
           </div>
         </div>
 
-
         <ul className="nav nav-tabs col-lg-8 mx-auto text-center d-flex justify-content-center">
           <li className="nav-item" id="0">
-            <a className={`nav-link ${pageNumber === 0 ? "active" : ""}`} id="0" aria-current="page" href="#" onClick={onChangePageNumber}>
+            <a
+              className={`nav-link ${pageNumber === 0 ? "active" : ""}`}
+              id="0"
+              aria-current="page"
+              href="#"
+              onClick={onChangePageNumber}
+            >
               프로젝트
             </a>
           </li>
           <li className="nav-item" id="1">
-            <a className={`nav-link ${pageNumber === 1 ? "active" : ""}`} href="#" id="1" onClick={onChangePageNumber}>
+            <a
+              className={`nav-link ${pageNumber === 1 ? "active" : ""}`}
+              href="#"
+              id="1"
+              onClick={onChangePageNumber}
+            >
               질문
             </a>
           </li>
           <li className="nav-item" id="2">
-            <a className={`nav-link ${pageNumber === 2 ? "active" : ""}`} href="#" id="2" onClick={onChangePageNumber}>
+            <a
+              className={`nav-link ${pageNumber === 2 ? "active" : ""}`}
+              href="#"
+              id="2"
+              onClick={onChangePageNumber}
+            >
               팀원
             </a>
           </li>
           <li className="nav-item" id="3">
-            <a className={`nav-link ${pageNumber === 3 ? "active" : ""}`} id="3" onClick={onChangePageNumber}>
+            <a
+              className={`nav-link ${pageNumber === 3 ? "active" : ""}`}
+              id="3"
+              onClick={onChangePageNumber}
+            >
               매니저
             </a>
           </li>
         </ul>
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 3329cfe (get accessToken, refreshToken)
-        <DetailPage 
+        <DetailPage
           changingPage={changingPage}
           project={project}
           pageNumber={pageNumber}
@@ -108,8 +115,6 @@ const Project = () => {
           creatingComment={creatingComment}
           onChangeComment={onChangeComment}
         />
-
-
       </>
     );
   }
@@ -125,71 +130,53 @@ const DetailPage = ({
   comments,
   onChangeComment,
   creatingComment,
-  creatingToDoListEle
+  creatingToDoListEle,
 }) => {
-  if(changingPage)
-  {
-    return(
-      <div>Loading...</div>
-    )
-  }
-  else
-  {
-    if(pageNumber === 0)
-    {
-      return(
-        <DetailPageZero
-          project={project}
-        />
-      )
-    }
-    else if(pageNumber === 1)
-    {
-      return(
-        <DetailPageOne 
+  if (changingPage) {
+    return <div>Loading...</div>;
+  } else {
+    if (pageNumber === 0) {
+      return <DetailPageZero project={project} />;
+    } else if (pageNumber === 1) {
+      return (
+        <DetailPageOne
           comment={comment}
           comments={comments}
           creatingComment={creatingComment}
           onChangeComment={onChangeComment}
         />
-      )
-    }
-    else if(pageNumber === 2)
-    {
-      return(
-        <DetailPageTwo 
+      );
+    } else if (pageNumber === 2) {
+      return (
+        <DetailPageTwo
           discordURL={discordURL}
           todoList={todoList}
           creatingToDoListEle={creatingToDoListEle}
         />
-      )
+      );
     }
   }
-}
+};
 
-const DetailPageTwo = ({
-  discordURL,
-  todoList,
-  creatingToDoListEle
-}) => {
-  return(
+const DetailPageTwo = ({ discordURL, todoList, creatingToDoListEle }) => {
+  return (
     <div className="container px-5">
       <div className="text-uppercase-expanded small mb-2 pt-5">
         <h4>To Do List</h4>
       </div>
       <hr class="mt-0 mb-3 mt-3 " />
-      {creatingToDoListEle ? (<></>) : (
-      <div>
-        {todoList.map((toDoListEle, index) => {
-          return(
-            <div>
-              <p key={index}>
-                {toDoListEle.task}
-              </p>
-            </div>
-          )
-        })}
-      </div>
+      {creatingToDoListEle ? (
+        <></>
+      ) : (
+        <div>
+          {todoList.map((toDoListEle, index) => {
+            return (
+              <div>
+                <p key={index}>{toDoListEle.task}</p>
+              </div>
+            );
+          })}
+        </div>
       )}
       <div className="text-uppercase-expanded small mb-2 pt-5">
         <h4>Community</h4>
@@ -199,133 +186,64 @@ const DetailPageTwo = ({
         <a href={discordURL}>Discord</a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const DetailPageOne = ({
   comment,
   comments,
   onChangeComment,
-  creatingComment
+  creatingComment,
 }) => {
-  return(
+  return (
     <div className="container px-5">
       <div className="text-uppercase-expanded small mb-2 pt-5">
         <h4>질문하기</h4>
       </div>
       <hr class="mt-0 mb-3 mt-3 " />
       <div className="form-floating">
-        <CommentInput 
-          comment={comment}
-          onChangeComment={onChangeComment}
-        />
-<<<<<<< HEAD
+        <CommentInput comment={comment} onChangeComment={onChangeComment} />
       </div>
-      <CommentList 
-        creatingComment={creatingComment}
-        comments={comments}
-      />
-
-
-    </div>
-  )
-}
-
-
-
-
-const DetailPageZero = ({
-  project
-}) => {
-  return(
-    <>
-  <div class="container px-5">
-    <div class="text-uppercase-expanded small mb-2 pt-5">
-      <h4>모집 현황</h4>
-    </div>
-    <hr class="mt-0 mb-3 mt-3 " />
-    <div class="row gx-5 mb-3 mt-3">
-      <div class="col-lg-8">
-        <h4 class="mb-0">Support field</h4>
-        <br />
-        <div class="support-fields">
-          {project.fields.map((p) => {
-            return (
-              <div className="row support-field">
-                <div className="col-md-5 support-field-label">
-                  {p.field}
-                </div>
-                <div className="col-md-5 support-field-value">
-                  {p.recruited}/{p.limit}
-                </div>
-              </div>
-            );
+      {creatingComment ? (
+        <></>
+      ) : (
+        <div>
+          {comments.map((comment, index) => {
+            return <div>{comment.comment}</div>;
           })}
         </div>
-=======
->>>>>>> parent of 3329cfe (get accessToken, refreshToken)
-      </div>
-      {creatingComment ? (<></>) : (
-      <div>
-        {comments.map((comment, index) => {
-          return(
-            <div>
-              {comment.comment}
-            </div>
-          )
-        })}
-      </div>
-      )}
-
-    </div>
-  )
-}
-
-
-
-
-const DetailPageZero = ({
-  project
-}) => {
-  return(
-    <>
-  <div class="container px-5">
-    <div class="text-uppercase-expanded small mb-2 pt-5">
-      <h4>모집 현황</h4>
-    </div>
-    <hr class="mt-0 mb-3 mt-3 " />
-    <div class="row gx-5 mb-3 mt-3">
-      <div class="col-lg-8">
-        <h4 class="mb-0">Support field</h4>
-        <br />
-        <div class="support-fields">
-          {project.fields.map((p) => {
-            return (
-              <div className="row support-field">
-                <div className="col-md-5 support-field-label">
-                  {p.field}
-                </div>
-                <div className="col-md-7 support-field-value">
-                  {p.recruited}/{p.limit}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-<<<<<<< HEAD
       )}
     </div>
   );
 };
 
-    </div>
-    <div class="text-uppercase-expanded small mb-2 pt-5">
-      <h4>소개</h4>
-    </div>
-    <hr class="mt-0 mb-3 mt-3 " />
-    <div class="row gx-5 mb-3 mt-3">
-      <div class="col-lg-8">
-        <h4 class="mb-0">1. 지원동기</h4>
+const DetailPageZero = ({ project }) => {
+  return (
+    <>
+      <div class="container px-5">
+        <div class="text-uppercase-expanded small mb-2 pt-5">
+          <h4>모집 현황</h4>
+        </div>
+        <hr class="mt-0 mb-3 mt-3 " />
+        <div class="row gx-5 mb-3 mt-3">
+          <div class="col-lg-8">
+            <h4 class="mb-0">Support field</h4>
+            <br />
+            <div class="support-fields">
+              {project.fields.map((p) => {
+                return (
+                  <div className="row support-field">
+                    <div className="col-md-5 support-field-label">
+                      {p.field}
+                    </div>
+                    <div className="col-md-7 support-field-value">
+                      {p.recruited}/{p.limit}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
 
           <div class="col-lg-4 text-lg-end">
             <div class="text-gray-400 small">May 2018 - Present</div>
@@ -372,63 +290,8 @@ const DetailPageZero = ({
           </div>
         </div>
       </div>
-=======
-      </div>
-
-      <div class="col-lg-4 text-lg-end">
-        <div class="text-gray-400 small">May 2018 - Present</div>
-      </div>
-    </div>
-    <div class="text-uppercase-expanded small mb-2 pt-5">
-      <h4>소개</h4>
-    </div>
-    <hr class="mt-0 mb-3 mt-3 " />
-    <div class="row gx-5 mb-3 mt-3">
-      <div class="col-lg-8">
-        <h4 class="mb-0">1. 지원동기</h4>
-
-        <p>{project.description}</p>
-      </div>
-      <div class="col-lg-4 text-lg-end">
-        <div class="text-gray-400 small">August 2015 - May 2018</div>
-      </div>
->>>>>>> parent of 3329cfe (get accessToken, refreshToken)
-    </div>
-    <div class="text-uppercase-expanded small mb-2 pt-5">
-      <h4>기술/언어</h4>
-    </div>
-    <hr class="mt-0 mb-3 mt-3 " />
-    <div class="row gx-5 mb-3 mt-3">
-      <div class="col-lg-8">
-        <h4 class="mb-0">구현하는데 필요한 스택</h4>
-        <br />
-        <p>{project.needs}</p>
-      </div>
-<<<<<<< HEAD
-=======
-      <div class="col-lg-4 text-lg-end">
-        <div class="text-gray-400 small">May 2018 - Present</div>
-      </div>
->>>>>>> parent of 3329cfe (get accessToken, refreshToken)
-    </div>
-    <div class="text-uppercase-expanded small mb-2 pt-5">
-      <h4>참고 링크</h4>
-    </div>
-    <hr class="mt-0 mb-3 mt-3 " />
-    <div class="row gx-5">
-      <div class="col-lg-8">
-        <p>{project.reference}</p>
-      </div>
-<<<<<<< HEAD
-=======
-      <div class="col-lg-4 text-lg-end">
-        <div class="text-gray-400 small">June 2011 - August 2015</div>
-      </div>
->>>>>>> parent of 3329cfe (get accessToken, refreshToken)
-    </div>
-  </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
 export default Project;
