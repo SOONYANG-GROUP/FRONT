336e96330931484e11ca7bc62b12463176bcc4df
Picka <marmong@naver.com>
2023-03-21 07:44:29 +0900

    Revert "Merge branch 'master' of https://github.com/SOONYANG-GROUP/FRONT"
    This reverts commit 44c0a69e5bb0b39f302e93e66d348b6b8a4947c2, reversing
    changes made to 0d7fac6e8b5805cbfa0039dd0c3019232dc12fe0.

--- src/Components/Modal/LoginModal.js
+++ src/Components/Modal/LoginModal.js
@@ -1,102 +1,49 @@
-import axios from "axios";
 import React from "react";
 
-const config = {
-  headers: {
-    "Content-Type": "application/json; charset=utf-8",
-  },
-};
-
-const responseGoogle = async (response) => {
-  console.log(1, response);
-  let jwtToken = await axios.post(
-    "http://localhost:8080/oauth/jwt/google",
-    JSON.stringify(response),
-    config
-  );
-  if (jwtToken.status === 200) {
-    console.log(2, jwtToken.data);
-    localStorage.setItem("jwtToken", jwtToken.data);
-  }
-};
-
 export const LoginModalBtn = () => {
-  return (
-    <>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        로그인
-      </button>
-      <LoginModal />
-    </>
-  );
-};
+    return(
+        <>
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            로그인
+        </button>
+        <LoginModal />
+        </>
+    )
+
+}
 
 const LoginModal = () => {
-  const HandleButtonClick = async (e) => {
-    console.log("asd");
-    axios
-      .post("https://localhost:8080/login")
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  return (
-    <div
-      className="modal fade"
-      id="exampleModal"
-      tabIndex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-body">
-            <h4 className="text-center">로그인 하기</h4>
-            <div>
-              <div>
-                <button
-                  className="w-100 mb-3 btn btn-primary"
-                  onClick={HandleButtonClick}
-                >
-                  <span className="fs-5">
-                    <i className="fa-brands fa-google"></i> 구글 로그인하기
-                  </span>
-                </button>
-              </div>
-              <div>
-                <button className="w-100 mb-3 btn btn-success">
-                  <span className="fs-5">
-                    <i className="fa-solid fa-n"></i> 네이버 로그인하기
-                  </span>
-                </button>
-              </div>
-              <div>
-                <button className="w-100 mb-3 btn btn-dark">
-                  <span className="fs-5">
-                    <i className="fa-brands fa-github"></i> 깃헙 로그인하기
-                  </span>
-                </button>
-              </div>
-              <div>
-                <button className="w-100 btn btn btn-warning">
-                  <span className="fs-5">
-                    <i className="fa-solid fa-comment"></i> 카카오 로그인하기
-                  </span>
-                </button>
-              </div>
+    return(
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-body">
+                    <h4 className="text-center">로그인 하기</h4>
+                    <div>
+                        <div>
+                            <button className="w-100 mb-3 btn btn-primary">
+                                <span className="fs-5"><i className="fa-brands fa-google"></i> 구글 로그인하기</span>
+                            </button>
+                        </div>
+                        <div>
+                            <button className="w-100 mb-3 btn btn-success">
+                                <span className="fs-5"><i className="fa-solid fa-n"></i> 네이버 로그인하기</span>
+                            </button>
+                        </div>
+                        <div>
+                            <button className="w-100 mb-3 btn btn-dark">
+                                <span className="fs-5"><i className="fa-brands fa-github"></i> 깃헙 로그인하기</span>
+                            </button>
+                        </div>
+                        <div>
+                            <button className="w-100 btn btn btn-warning">
+                                <span className="fs-5"><i className="fa-solid fa-comment"></i> 카카오 로그인하기</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
