849faede68304fa9af1507c815086e9025eb6f38
Picka <marmong@naver.com>
2023-03-20 10:29:50 +0900

    Update Skill.js

--- src/Pages/Roadmap/Skill.js
+++ src/Pages/Roadmap/Skill.js
@@ -2,25 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import frontendDeveloper from "../../assets/images/frontendDeveloper.svg";
 
-import RoadmapDummyData from "../../DummyData/Roadmap.json";
+import SkillsDummyData from "../../DummyData/Skills.json";
 
 const Skill = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [roadmap, setRoadmap] = useState(null);
+  const [skill, setSkill] = useState(null);
   const id = useParams().id;
 
   useEffect(() => {
-    if (GetRoadmap(id)) {
-      setRoadmap(GetRoadmap(id));
+    if (GetSkill(id)) {
+      setSkill(GetSkill(id));
       setIsLoading(false);
     }
   }, [id]);
 
-  const GetRoadmap = (id) => {
-    return RoadmapDummyData.roadmaps[id - 1];
+  const GetSkill = (id) => {
+    return SkillsDummyData.skills[id - 1];
   };
 
   if (!isLoading) {
+    console.log(skill);
     return (
       <div>
         <header class="page-header-ui page-header-ui-dark bg-gradient-primary-to-secondary">
@@ -31,7 +32,7 @@ const Skill = () => {
                   class="col-lg-6 aos-init aos-animate"
                   data-aos="fade-right"
                 >
-                  <h1 class="page-header-ui-title">{roadmap.roadmap}란?</h1>
+                  <h1 class="page-header-ui-title">{skill.name}란?</h1>
                   <p class="page-header-ui-text mb-5">
                     로그인 기능을 통해 해당 내용을 수정할 수 있습니다.
                   </p>
@@ -57,11 +58,7 @@ const Skill = () => {
                       data-color="black"
                     >
                       <div class="screen">
-                        <img
-                          class="img-fluid"
-                          src={frontendDeveloper}
-                          alt="..."
-                        />
+                        <img class="img-fluid" src={skill.imageUrl} alt="..." />
                       </div>
                     </div>
                   </div>
@@ -77,17 +74,17 @@ const Skill = () => {
               <div class="col-lg-4 mb-5 mb-lg-0">
                 <h3>평균 연봉</h3>
                 <br />
-                <p class="mb-0 fs-1">{roadmap.averageSalary}</p>
+                asd
               </div>
               <div class="col-lg-4 mb-5 mb-lg-0">
                 <h3>대표 언어</h3>
                 <br />
-                <p class="mb-0 fs-1">{roadmap.mostUsedLanguage}</p>
+                asd
               </div>
               <div class="col-lg-4 mb-5 mb-lg-0">
                 <h3>대표 프레임 워크</h3>
                 <br />
-                <p class="mb-0 fs-1">{roadmap.mostUsedFramework}</p>
+                asd
               </div>
             </div>
           </div>
@@ -95,42 +92,6 @@ const Skill = () => {
         <br />
         <br />
         <br />
-
-        <section class="bg-white py-10" id="get-started">
-          <div class="container px-5">
-            <div class="row gx-5 text-center">
-              <div class="text-uppercase-expanded small mb-2 pt-5 pb-5">
-                <h4>필요한 기술 스택</h4>
-              </div>
-              {roadmap.skills.map((s) => {
-                return (
-                  <div class="col-lg-4 mb-5 mb-lg-0">
-                    <div class="icon-stack icon-stack-xl bg-gradient-primary-to-secondary text-white mb-4">
-                      <img
-                        src={s.skillImgUrl}
-                        alt="..."
-                        width="70"
-                        height="70"
-                      />
-                    </div>
-                    <h2>{s.name}</h2>
-                    <p class="mb-0">{s.description}</p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div class="svg-border-rounded text-light">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 144.54 17.34"
-              preserveAspectRatio="none"
-              fill="currentColor"
-            >
-              <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path>
-            </svg>
-          </div>
-        </section>
       </div>
     );
   }
