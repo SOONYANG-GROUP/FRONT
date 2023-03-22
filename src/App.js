0f6589f37608030ed98f5ceb26a4be37afc337f4
Picka <marmong@naver.com>
2023-03-22 21:25:43 +0900

    nn

--- src/App.js
+++ src/App.js
@@ -23,107 +23,68 @@ import EditRoadmap from "./Pages/Roadmap/EditRoadmap";
 import EditSkill from "./Pages/Roadmap/EditSkill";
 
 import axios from "axios";
+import TestNode from "./TestNode";
+
 function App() {
-  let refreshSubscribing = false;
-  console.log("App.js");
-  if (!window.sessionStorage.getItem("accessToken")) {
-    console.log("로그인이 필요합니다.");
-  } else {
-    axios.interceptors.request.use(
-      async (config) => {
-        const accessToken = await window.sessionStorage.getItem("accessToken");
-        // console.log(accessToken);
-
-        if (!accessToken) {
-          console.log("accessToken이 없습니다.");
-          return;
-        } else {
-          console.log("accessToken이 있습니다.");
-          // console.log(accessToken);
-          config.headers["Authorization"] = await accessToken;
-        }
-
-        return config;
-      },
-      (error) => Promise.reject(error)
-    );
-
-    axios.interceptors.response.use(
-      (response) => response, // 응답이 성공적인 경우 아무것도 하지 않음
-      async (error) => {
-        // 액세스 토큰이 만료됐다면
-        error.preventDefault();
-        console.log("액세스 토큰이 만료됐다면");
-        console.log(error);
-        if (error.response.status === 401) {
-          console.log("401 에러");
-          if (!refreshSubscribing) {
-            refreshSubscribing = true;
-            console.log("refresh true변경");
-            const response = await postRefreshToken(); // 액세스토큰 갱신
-            console.log("액세스토큰 갱신");
-
-            // 갱신된 accessToken을 받으면
-            if (response) {
-              window.sessionStorage.setItem(
-                "accessToken",
-                response.accessToken
-              ); // 새로운 토큰 sessionStorage 저장
-              window.sessionStorage.setItem(
-                "refreshToken",
-                response.refreshToken
-              );
-              refreshSubscribing = false;
-              console.log("refreshtoken false로 변경");
-              error.config.headers["Authorization"] = response; // 원래 api 요청의 headers의 accessToken도 변경
-              const originalResponse = axios.request(error.config); // 원래 api 요청하기
-              return originalResponse; // 원래 api 요청의 response return
-            }
-            // 리프레시 토큰도 만료됐으면 로그인 페이지로 이동
-            else {
-              // window.sessionStorage.clear();
-              // window.location.href = "/login";
-            }
-          }
-        }
-
-        return Promise.reject(error);
+  axios.interceptors.request.use(
+    async (config) => {
+      console.log(config);
+      const accessToken = await window.sessionStorage.getItem("accessToken");
+      if (accessToken) {
+        config.headers["Authorization"] = await accessToken;
+        console.log(config);
       }
-    );
-
-    async function postRefreshToken() {
-      try {
-        // 리프레쉬 토큰을 이용해 액세스 토큰을 갱신
-        const refreshToken = window.sessionStorage.getItem("refreshToken");
-        const headers = {
-          "Content-Type": "application/json;charset=utf-8",
-          "Authorization-refresh": refreshToken,
-        };
-        console.log("리프레쉬 토큰을 이용해 액세스 토큰을 갱신");
-
-        const { data } = await axios.post(
-          `http://localhost:8080/jwt-test`,
-          {},
-          {
-            headers: headers,
-          }
-        );
-
-        // 서버로부터 받은 데이터
-        if (data.flag === "success") {
-          console.log("서버로부터 받은 데이터 success");
-          return data.data[0];
-        } else if (data.flag === "fail") {
-          console.log("서버로부터 받은 데이터 false");
-          return false;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
-      }
+      return config;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject(error);
     }
-  }
+  );
+
+  axios.interceptors.response.use(
+    async (response) => {
+      return response;
+    }, // 응답이 성공적인 경우 아무것도 하지 않음
+    async (error) => {
+      console.log(error);
+      const {
+        config,
+        response: { status },
+      } = error;
+
+      if (status === 401) {
+        console.log("401");
+        const originalRequest = config;
+
+        const refreshToken = await sessionStorage.getItem("refreshToken");
+        console.log(`refreshToken : ${refreshToken}`);
+        const { data } = await axios
+          .post(
+            "http://localhost:8080/jwt-test",
+            {},
+            {
+              headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                Authorizationrefresh: refreshToken,
+              },
+            }
+          )
+          .then((res) => {
+            console.log(res.config.headers.Authorization);
+            console.log(res.config.headers[0]);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        sessionStorage.setItem("accessToken", data.data.accessToken);
+        originalRequest.headers.Authorization = `${data.data.accessToken}`;
+        return axios(originalRequest);
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return (
     <Router>
@@ -142,10 +103,10 @@ function App() {
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="/edit/skill/:id" element={<EditSkill />} />
         <Route path="/edit/roadmap/:id" element={<EditRoadmap />} />
-
         <Route path="/test" element={<Test />} />
         <Route path="/test2" element={<Test2 />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/testnode" element={<TestNode />} />
       </Routes>
     </Router>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,15 +3,13 @@ import { useEffect, useState } from "react";
 
 const Login = () => {
   const params = new URLSearchParams(window.location.search);
-  const [ accessToken, setAccessToken ] = useState(params.get("accessToken"));
-  const [ refreshToken, setRefreshToken ] = useState(params.get("refreshToken"));
-  
+  const [accessToken, setAccessToken] = useState(params.get("accessToken"));
+  const [refreshToken, setRefreshToken] = useState(params.get("refreshToken"));
 
   sessionStorage.setItem("accessToken", accessToken);
   sessionStorage.setItem("refreshToken", refreshToken);
 
-  /* 
-  useEffect(() => {
+  const onclickButton = () => {
     const fetch = async () => {
       console.log("요청");
       await axios
@@ -20,45 +18,15 @@ const Login = () => {
           console.log("response입니다 ", response);
         })
         .catch((e) => {
-          console.error(e.message);
-          // const throwException = async () => {
-          //   console.log("throwExceoption");
-          //   await axios
-          //     .post("http://localhost:8080/jwt-test", null, {
-          //       headers: {
-          //         "Content-Type": "application/json",
-          //         "Authorization-refresh": refreshToken,
-          //         Authorization: accessToken,
-          //       },
-          //     })
-          //     .then((res) => {
-          //       console.log(res);
-          //     });
-          // };
-          // throwException();
-          // handle error
+          console.log(e);
         });
     };
     fetch();
-  }, []);
-  */
-  const onSendToken = async (e) => {
-    e.preventDefault();
-    await axios
-      .post("http://localhost:8080/jwt-test", null, {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization-refresh": refreshToken,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-      });
   };
 
   return (
     <>
-      <button onClick={onSendToken}>send refreshToken</button>
+      <button onClick={onclickButton}>send refreshToken</button>
     </>
   );
 };
