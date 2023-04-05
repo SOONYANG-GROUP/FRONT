30163832fef72e8ee8fed31ffb61349b91222e53
coldsteelpope <coldsteelpope@gmail.com>
2023-04-05 17:41:29 +0900

    내를 많이 닮은 내 손주

--- src/Pages/VideoTest.js
+++ src/Pages/VideoTest.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 
 
 //const backendURLForSocket = "http://localhost:5000";
-const testBackendURLForSocket = "https://yummy-shrimps-grow-39-127-110-132.loca.lt";
+const testBackendURLForSocket = "https://puny-socks-occur-39-127-110-132.loca.lt/";
 
 let roomName = ""
 let myStream = {};
