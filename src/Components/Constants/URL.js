941584d904d5afc0ae4ca178f087b959c910b443
coldsteelpope <coldsteelpope@gmail.com>
2023-05-04 16:19:05 +0900

    change backend url

--- src/Components/Constants/URL.js
+++ src/Components/Constants/URL.js
@@ -1,3 +1,4 @@
 // export const SUB_BACK_URL = "http://localhost:9999";
-export const BACK_URL = "http://localhost:8080";
+//export const BACK_URL = "http://localhost:8080";
+export const BACK_URL = "http://52.91.214.195"
 export const SUB_BACK_URL = "https://soonyang-group.onrender.com";
